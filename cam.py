b1d60033ff3359c22c7a3f2a0794436e8de5a5e8
mkarpik <marcin.karpik@gmail.com>
2021-01-22 12:56:19 -0800

    working test

--- cam.py
+++ cam.py
@@ -1,1 +1,11 @@
-import 
+from gpiozero import MotionSensor
+from picamera import PiCamera
+
+pir = MotionSensor(4)
+camera = PiCamera()
+
+while True:
+    pir.wait_for_motion()
+    if pir.motion_detected:
+        print("Good Looking Person Detected")
+        camera.start_preview()
