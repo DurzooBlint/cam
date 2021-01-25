cedaa2c2a6416e7620fda515729d42c537a1dfd2
mkarpik <marcin.karpik@gmail.com>
2021-01-24 17:34:58 -0800

    configuration from file sending email

--- messenger.py
+++ messenger.py
@@ -1,7 +1,9 @@
 import smtplib
-import time
+import ssl
 import datetime
 import configparser
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 
 class Email:
@@ -19,24 +21,58 @@ class Email:
         self.GMAIL_PASSWORD = config['DEFAULT']['GMAIL_PASSWORD']
 
     def send_email(self):
-        # Create Headers
-        headers = ["From: " + self.GMAIL_USERNAME, "Subject: " + self.subject, "To: " + self.recipient,
-                   "MIME-Version: 1.0", "Content-Type: text/html"]
-        headers = "\r\n".join(headers)
+        # # Create Headers
+        # headers = ["From: " + self.GMAIL_USERNAME, "Subject: " + self.subject, "To: " + self.recipient,
+        #            "MIME-Version: 1.0", "Content-Type: text/html"]
+        # headers = "\r\n".join(headers)
+        #
+        # # Connect to Gmail Server
+        # session = smtplib.SMTP(self.SMTP_SERVER, self.SMTP_PORT)
+        # session.ehlo()
+        # session.starttls()
+        # session.ehlo()
+        #
+        # # Login to Gmail
+        # session.login(self.GMAIL_USERNAME, self.GMAIL_PASSWORD)
+        #
+        # # Send Email & Exit
+        # session.sendmail(self.GMAIL_USERNAME, self.recipient, headers + "\r\n\r\n" + self.body)
+        # session.quit
 
-        # Connect to Gmail Server
-        session = smtplib.SMTP(self.SMTP_SERVER, self.SMTP_PORT)
-        session.ehlo()
-        session.starttls()
-        session.ehlo()
+        message = MIMEMultipart("alternative")
+        message["Subject"] = "Guardian alert: Activity detected"
+        message["From"] = self.GMAIL_USERNAME
+        message["To"] = self.recipient
 
-        # Login to Gmail
-        session.login(self.GMAIL_USERNAME, self.GMAIL_PASSWORD)
+        # Create the plain-text and HTML version of your message
+        html = """\
+        <html>
+          <body>
+            <p>
+                <b>Guardian Alert</b>
+            </p>
+          </body>
+        </html>
+        """
 
-        # Send Email & Exit
-        session.sendmail(self.GMAIL_USERNAME, self.recipient, headers + "\r\n\r\n" + self.body)
-        session.quit
+        # Turn these into plain/html MIMEText objects
+        part1 = MIMEText(self.body, "plain")
+        part2 = MIMEText(html, "html")
+
+        # Add HTML/plain-text parts to MIMEMultipart message
+        # The email client will try to render the last part first
+        message.attach(part1)
+        message.attach(part2)
+
+        # Create secure connection with server and send email
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(self.SMTP_SERVER, self.SMTP_PORT, context=context) as server:
+            server.login(self.GMAIL_USERNAME, self.GMAIL_PASSWORD)
+            server.sendmail(
+                self.GMAIL_USERNAME, self.recipient, message.as_string()
+            )
 
     def log_email(self):
         with open('messaging.log', 'a', encoding='utf-8') as f:
-            f.write(f"{datetime.datetime.now().strftime('%Y-%m-%d_%H.%M.%S')}: Recipient:{self.recipient}, Subject:{self.subject}")
+            f.write(
+                f"{datetime.datetime.now().strftime('%Y-%m-%d_%H.%M.%S')}: Recipient:{self.recipient}, Subject:{self.subject}")
