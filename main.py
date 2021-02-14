d1aa9737ede123ffc90e187b01708f2c90c9d156
mkarpik <marcin.karpik@gmail.com>
2021-02-14 15:16:40 -0800

    Moved camera functionality into new camwrapper class

--- main.py
+++ main.py
@@ -5,14 +5,23 @@ import datetime
 import messenger
 import camwrapper
 
-# GPIO-4 (Pin 7) - PIR sensor
+# GPIO-4 (Pin 7) - PIR sensor.
 pir = MotionSensor(4)
 
 
+# create directories and configuration files.
+def initialize():
+    directory = "video/"
+    if not os.path.isdir(directory):
+        os.makedirs(directory)
+
+
+# get filename based on current date time.
 def get_file_name():
     return datetime.datetime.now().strftime("video/%Y-%m-%d_%H.%M.%S.h264")
 
 
+# ensure there is no more than 6 latest files stored.
 def clean_storage():
     recordings = os.listdir("video/")
     if len(recordings) > 5:
@@ -22,13 +31,16 @@ def clean_storage():
             print(f"Failed to delete {recordings[len(recordings) - 1]}. Reason: {e}")
 
 
+# write event into log file
 def log_event(event):
     print(event)
     with open('activity.log', 'a', encoding='utf-8') as f:
         f.write(event + '\n')
 
 
+# main program function
 def main():
+    initialize()
     print("Monitoring for activity...")
     while True:
         filename = get_file_name()
